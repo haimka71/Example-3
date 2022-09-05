7f0093558948b7fad338522caff69d37cb602c75
«Anatoly <haimka71@mail.ru>
2022-09-05 21:00:12 +0300

    4

--- Program.cs
+++ Program.cs
@@ -5,8 +5,44 @@
 78 -> 8 12-> 2 85 -> 8
 10 99
 */
-Random random = new Random();
-int randomValue = random.Next(10, 100);
+// Random random = new Random();
+// int randomValue = random.Next(10, 100);
+
+int number = GetMaxNumberRandomValue();
+
+Console.WriteLine(number);
+
+
+void GetMaxNumberRandomValue()
+{
+    int randomValue = new Random().Next(10, 100);
+
+    Console.WriteLine($"Random value - {randomValue}");
+
+    int lastNumber = randomValue % 10;
+    int firstNumber = randomValue / 10;
+
+    int max = lastNumber;
+
+
+    if (lastNumber == firstNumber)
+    {
+        Console.WriteLine($"Числа равны!");
+    }
+    else
+    {
+        if (max < firstNumber)
+        {
+            max = firstNumber;
+        }
+        Console.WriteLine($"Max = {max}");
+    }
+}
+return 12;
+
+
+
+int randomValue = new Random().Next(10, 100);
 
 Console.WriteLine($"Random value - {randomValue}");
 
