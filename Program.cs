85b1b1261eac14b1f9c463bdb6a248038fd68265
«Anatoly <haimka71@mail.ru>
2022-09-11 13:38:48 +0300

    Seminar 3 Task 21

--- Program.cs
+++ Program.cs
@@ -243,38 +243,73 @@
 	// 5 -> 1, 8, 27, 64, 125
 	
 
-	Console.Clear();
-	Console.Write("Введите число: ");
-	int cube = Convert.ToInt32(Console.ReadLine());
+	// Console.Clear();
+	// Console.Write("Введите число: ");
+	// int cube = Convert.ToInt32(Console.ReadLine());
 	
 
-	void Cube(int[] cube) // вводим не возвращающий метод Cube с аргументом в виде массива, имя cube
-    {
-	  int counter = 0;
-	  int length = cube.Length;
-	  while (counter <  length)
-      {
-	    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3)); //MathPow метод Возвращает указанное число (counter), возведенное в указанную степень (3).
-	    counter++;
-	  }
+	// void Cube(int[] cube) // вводим не возвращающий метод Cube с аргументом в виде массива, имя cube
+    // {
+	//   int counter = 0;
+	//   int length = cube.Length;
+	//   while (counter <  length)
+    //   {
+	//     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3)); //MathPow метод Возвращает указанное число (counter), возведенное в указанную степень (3).
+	//     counter++;
+	//   }
+	// }
+	
+
+	// void PrintArray(int[] coll) // вводим не возвращающий метод PrintArray с аргументом в виде массива, имя coll
+    // {
+	//   int count = coll.Length;
+	//   int index = 0;
+	//   while(index < count)
+    //   {
+	//     Console.Write(coll[index]+ " "); // " " вводим пробел
+	//     index++;
+	//   }
+	// } 
+	
+	// int[] array = new int[cube+1];
+    
+	// Cube(array); // вызов метода Cube
+
+	// PrintArray(array); // вызов метода PrintArray
+
+
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+	// A (3,6,8); B (2,1,-7), -> 15.84
+	// A (7,-5, 0); B (1,-1,9) -> 11.53
+	
+
+    Console.Clear();
+	int x1 = Coordinate("x", "A");
+	int y1 = Coordinate("y", "A");
+	int z1 = Coordinate("z", "A");
+	int x2 = Coordinate("x", "B");
+	int y2 = Coordinate("y", "B");
+	int z2 = Coordinate("z", "B");
+	
+
+	int Coordinate(string coorName, string pointName)
+	{
+	    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+	    return Convert.ToInt16(Console.ReadLine());
 	}
 	
 
-	void PrintArray(int[] coll) // вводим не возвращающий метод PrintArray с аргументом в виде массива, имя coll
-    {
-	  int count = coll.Length;
-	  int index = 0;
-	  while(index < count)
-      {
-	    Console.Write(coll[index]+ " "); // " " вводим пробел
-	    index++;
-	  }
-	} 
+	double Result(double x1, double x2, double y1, double y2, double z1, double z2)
+	{
+	  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+	                   Math.Pow((y2-y1), 2) + 
+	                   Math.Pow((z2-z1), 2));
+	}
 	
-	int[] array = new int[cube+1];
-    
-	Cube(array); // вызов метода Cube
 
-	PrintArray(array); // вызов метода PrintArray
+	double LinesegmentLength =  Math.Round (Result(x1, x2, y1, y2, z1, z2), 2 );
+	
 
-
+	Console.WriteLine($"Длина отрезка  {LinesegmentLength}");
